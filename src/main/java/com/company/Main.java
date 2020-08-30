0b330b01ef82889bb8837ba683c939fad596e287
Ерофеевский Юрий Александрович <borlok@bk.ru>
2020-08-30 14:02:48 +0300

    Resource example

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,16 +1,30 @@
 package com.company;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.text.NumberFormat;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
-        Player player = (Player) context.getBean("player");
-        player.playMusic();
-        System.out.println(player.getVolume() + " " + player.getName());
+        Locale locale = new Locale("ru","RU");
+        String pat = "Today is {0,date,long}, My name is {1}, and i have {2,number,currency}";
+        ResourceBundle resource = ResourceBundle.getBundle("Language_ru_RU",locale);
+        NumberFormat format = NumberFormat.getCurrencyInstance(locale);
+        DateFormat dateFormat = DateFormat.getDateInstance(1,locale);
+
+        String arm = MessageFormat.format( pat,
+                new GregorianCalendar(2020,8,25).getTime(),
+                resource.getString("name"),
+                Integer.parseInt(resource.getString("price")));
+
+        System.out.println(arm);
+
+        double a = 12345.78;
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        System.out.println(format.format(a));
     }
 
 }
--- src/main/java/com/company/Music.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public interface Music {
-    void play();
-}
